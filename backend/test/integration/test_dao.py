a6183f1c4a7700594390b4ecf41d8dda4becd1a8
Anna Berg <anbj21@student.bth.se>
2023-04-24 11:12:58 +0200

    finalize assignment 3

--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -1,8 +1,11 @@
 import pytest
 import unittest.mock as mock
+from pymongo.errors import WriteError
 from unittest.mock import patch
+import json 
 
 from src.util.dao import DAO
+import json
 
 testValidator =     {
 "$jsonSchema": {
@@ -25,16 +28,18 @@ testValidator =     {
         }
     }
 }
+
 @pytest.fixture
 def sut():
-    with patch("src.util.dao.getValidator", autospec=True) as mockedGetValidators:
-        mockedGetValidators.return_value = testValidator 
+    with patch("src.util.dao.getValidator", autospec=True) as mockGetValidators:
+        mockGetValidators.return_value = testValidator
         task = DAO(collection_name="testTask")
 
         yield task
         task.collection.drop()
 
-'''Test if all required properties is present, valid bson data, unique items (only 1 item with same name in document)'''
+
+'''1 . Test to see if object is returned if all required properties is present, valid bson data, unique items (only 1 item with same name in document)'''
 @pytest.mark.integration
 @pytest.mark.parametrize('inputData, expected',
     [
@@ -46,30 +51,36 @@ def test_create(sut, inputData,  expected):
     assert type(valid_res) == expected
 
 """
-Test to see if exception is raised when the validator dont fulfill required properties: 
-
-Test if all required properties is present, invalid bson data, unique items
-Test if all required properties is not present, valid bson data, unique items
-Test if all required properties is not present, invalid bson data, unique items
-Test if all required properties is present, invalid bson data, not unique items
-Test if all required properties is present, valid bson data, not unique items
-Test if all required properties is not present, valid bson data, not unique items
-Test if all required properties is not present, invalid bson data, not unique items
+Test to see if exception is raised if: 
+[0] all required properties is present, INVALID BSON, unique items
+[1] all required properties is NOT PRESENT, valid bson data, unique items
+[2] all required properties is NOT PRESENT, invalid bson data, unique items
 """
-@pytest.mark.integration
 @pytest.mark.parametrize('inputData, expected',
     [
-        ({"tile": "title", "description": 1, "bool_value": True}, "WriteError"),
-        ({"tile": "title", "bool_value": True}, "WriteError"),
-        ({"tile": "title", "bool_value": 1}, "WriteError"),
-        ({"tile": "", "description": "descrition", "bool_value": True}, "WriteError"),
-        ({"tile": "", "description": "descrition", "bool_value": True}, "WriteError"),
-        ({"tile": "", "bool_value": True}, "WriteError"),
-        ({"tile": "", "bool_value": 1}, "WriteError"),
+        ({"title": "title", "description": 1, "bool_value": True}, "WriteError"),
+        ({"title": "title", "bool_value": True}, "WriteError"),
+        ({"title": "title", "bool_value": "hej"}, "WriteError"),           
     ]
 )
 @pytest.mark.integration
 def test_invalid_writeerror(sut, inputData, expected):
-    with pytest.raises(Exception) as WriteError:
+    with pytest.raises(WriteError):
         sut.create(inputData)
+    assert expected in str(WriteError)
+
+"""
+Test to see if all properties is present, valid bson and not unique item.
+dao.js need correction before all test scenarios can be implemented.
+"""
+@pytest.mark.parametrize('inputData, expected',
+    [
+        ([{"title": "title", "description": "right", "bool_value": True}, {"title": "title", "description": "right", "bool_value": True}], "WriteError"), 
+    ]
+)
+@pytest.mark.integration
+def test_invalid_uniqueItems(sut, inputData, expected):
+    with pytest.raises(WriteError):
+        for item in inputData:
+            sut.create(item)
     assert expected in str(WriteError)
